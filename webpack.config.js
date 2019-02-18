901eeade8367fb73fd656b0d233c308437a405e5
Ahmed AbdelHalim <ahmed@i.graphics>
2019-02-18 15:52:46 +0100

    Reorder the keys alphabetically and turn off minimizing code

--- webpack.config.js
+++ webpack.config.js
@@ -8,22 +8,26 @@ Object.keys(slsw.lib.entries).forEach(
 );
 
 module.exports = {
-  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
-  entry: entries,
   devtool: 'source-map',
-  resolve: {
-    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
-  },
-  output: {
-    libraryTarget: 'commonjs',
-    path: path.join(__dirname, '.webpack'),
-    filename: '[name].js',
-  },
-  target: 'node',
+  entry: entries,
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
   module: {
     rules: [
       // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
       { test: /\.tsx?$/, loader: 'ts-loader' },
     ],
   },
+  optimization: {
+    // Turn off minimization of code
+    minimize: false
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+  },
+  target: 'node',
 };
