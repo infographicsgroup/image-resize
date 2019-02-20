3e19b71932c3bc5378981e95029b9a53be6b7e65
Ahmed AbdelHalim <ahmed@i.graphics>
2019-02-20 18:33:13 +0100

    Add the absolute and relative API for generating the images

--- src/absoluteWidthPreview.ts
+++ src/absoluteWidthPreview.ts
@@ -1,6 +1,61 @@
-import { S3EventRecord } from 'aws-lambda';
+import { S3EventRecord } from "aws-lambda";
+import imageFactory from "./utils/imageFactory";
 
-function generateAbsoluteWidthPreview(_event: S3EventRecord) {
+const SIZES = [
+  {
+    width: 4000,
+    height: null,
+    key: "w4000"
+  },
+  {
+    width: 3200,
+    height: null,
+    key: "w3200"
+  },
+  {
+    width: 2400,
+    height: null,
+    key: "w2400"
+  },
+  {
+    width: 2000,
+    height: null,
+    key: "w2000"
+  },
+  {
+    width: 1600,
+    height: null,
+    key: "w1600"
+  },
+  {
+    width: 1200,
+    height: null,
+    key: "w1200"
+  },
+  {
+    width: 1000,
+    height: null,
+    key: "w1000"
+  },
+  {
+    width: 800,
+    height: null,
+    key: "w800"
+  },
+  {
+    width: 600,
+    height: null,
+    key: "w600"
+  },
+  {
+    width: 400,
+    height: null,
+    key: "w400"
+  }
+];
+
+function generateAbsoluteWidthPreview(event: S3EventRecord) {
+  imageFactory(event, () => SIZES);
 }
 
 export default generateAbsoluteWidthPreview;
--- src/relativeAspectRatioPreview.ts
+++ src/relativeAspectRatioPreview.ts
@@ -1,6 +1,46 @@
 import { S3EventRecord } from 'aws-lambda';
+import imageFactory from "./utils/imageFactory";
 
-function generateRelativeAspectRatioPreview(_event: S3EventRecord) {
+function sizes(width: number, height: number) {
+  const aspectRatio = width / height;
+  const relativeSizes = [
+    {
+      width: (width * 3) / 16,
+      key: "3_16"
+    },
+    {
+      width: width / 4,
+      key: "1_4"
+    },
+    {
+      width: (width * 3) / 8,
+      key: "3_8"
+    },
+    {
+      width: width / 2,
+      key: "1_2"
+    },
+    {
+      width: (width * 3) / 4,
+      key: "3_4"
+    },
+    {
+      width: width,
+      key: "1"
+    }
+  ].map(({ width, key }) => {
+    return {
+      key,
+      // we need to have positive integers for sharp
+      width: Math.round(width),
+      height: Math.round(width / aspectRatio)
+    };
+  });
+  return relativeSizes;
+}
+
+function generateRelativeAspectRatioPreview(event: S3EventRecord) {
+  imageFactory(event, sizes);
 }
 
 export default generateRelativeAspectRatioPreview;
