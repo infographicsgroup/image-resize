293c22711670216fa88cfdb7ff9e355ec1505024
Ahmed AbdelHalim <ahmed@i.graphics>
2019-02-20 15:13:24 +0100

    Add the methods signature for generating the preview images

---
+++ src/absoluteWidthPreview.ts
@@ -0,0 +1,6 @@
+import { S3EventRecord } from 'aws-lambda';
+
+function generateAbsoluteWidthPreview(_event: S3EventRecord) {
+}
+
+export default generateAbsoluteWidthPreview;
---
+++ src/relativeAspectRatioPreview.ts
@@ -0,0 +1,6 @@
+import { S3EventRecord } from 'aws-lambda';
+
+function generateRelativeAspectRatioPreview(_event: S3EventRecord) {
+}
+
+export default generateRelativeAspectRatioPreview;
